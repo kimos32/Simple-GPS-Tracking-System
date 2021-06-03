146c21bc84b5962face923a062c7889be25713b2
yossef-Joe <85301193+yossef-Joe@users.noreply.github.com>
2021-06-03 18:06:18 +0200

    Add files via upload
    change  for loop into while loop

--- Distance.c
+++ Distance.c
@@ -4,7 +4,8 @@
 int main (void){
 
     int Distance;
-    
+		int i = 0;
+     port_init();
     for (Distance=0 ; Distance <= 0 ; Distance ++ ){
 
         if (Distance >= 100){
@@ -15,11 +16,12 @@ int main (void){
             LED &= ~0x08;
         }
         LCD_CMD(0x0C);              // set cursor to beginning of second row 
-        for(int i=0 ; i< 12 ; i++ ){
+				
+        while (i < 12){
             LCD_CMD(0x14);          // Shift cursor position to right
+					i++;
         }
         LCD_WRITE(Distance);    // write vlaue of Distance on the "LCD" 
     }
 
-
-}
+	}
