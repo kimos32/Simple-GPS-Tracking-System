eeeb0062bc26f3e23ec1f6cd69e299272fe48c6a
Karim Soltan <itzmeclassic@gmail.com>
2021-06-04 01:10:03 +0200

    Add files via upload
    edited ports from PF4 -> PB6

--- Distance.c
+++ Distance.c
@@ -6,7 +6,22 @@ int main (void){
     int Distance;
 		int i = 0;
      port_init();
-    for (Distance=0 ; Distance <= 0 ; Distance ++ ){
+	
+	       /*this function must be written as Keil 
+         allows running of a function before the main*/
+	      while(1) {
+        LCD_CMD(0X38);  //8-bit bus mode, 2 line display mode, 5x8 dots display mode
+        Delay();
+        LCD_CMD(0X01);  //clear display
+        Delay();
+        LCD_CMD(0X0C);  //display is on 
+        Delay();
+        LCD_CMD(0x80);  // set cursor to beginning of first row
+        Delay(); 
+		   	LCD_WRITE('K'); //send Distance on the LCD 
+        Delay();
+    
+				   for (Distance=0 ; Distance <= 0 ; Distance ++ ){
 
         if (Distance >= 100){
 
@@ -21,7 +36,11 @@ int main (void){
             LCD_CMD(0x14);          // Shift cursor position to right
 					i++;
         }
-        LCD_WRITE(Distance);    // write vlaue of Distance on the "LCD" 
+        LCD_WRITE(Distance);    // write value of Distance on the "LCD" 
     }
 
+				
+				}
+	
+ 
 	}
