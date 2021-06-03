8a907e558a8c72e682191c851d0fd6225e3d5bfb
Hisham5005 <85290215+Hisham5005@users.noreply.github.com>
2021-06-04 01:04:59 +0200

    Add files via upload

--- LCD.c
+++ LCD.c
@@ -19,10 +19,10 @@ void LCD_CURSOR (void);
 //this function passes the command to the LCD
 void LCD_CMD(unsigned long cmd) {
        
-    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x08) | ((cmd & 0x01 ) << 3 ); //pb3 = D0
-    GPIO_PORTC_DATA_R = (GPIO_PORTC_DATA_R & ~0xF0) | ((cmd & 0x1E ) << 3 ); //pc 4,5,6,7 = D 1,2,3,4
-    GPIO_PORTD_DATA_R = (GPIO_PORTD_DATA_R & ~0xC0) | ((cmd & 0x60 ) << 1 ); //PD 6,7 = D 5,6
-    GPIO_PORTF_DATA_R = (GPIO_PORTF_DATA_R & ~0x10) | ((cmd & 0x80 ) >> 3 ); //PF4 = D7
+    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x08) | ((cmd & 0x01 ) << 3 ); //PB3 = D0
+    GPIO_PORTC_DATA_R = (GPIO_PORTC_DATA_R & ~0xF0) | ((cmd & 0x1E ) << 3 ); //PC4,5,6,7 = D1,2,3,4
+    GPIO_PORTD_DATA_R = (GPIO_PORTD_DATA_R & ~0xC0) | ((cmd & 0x60 ) << 1 ); //PD6,7 = D5,6
+    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x40) | ((cmd & 0x80 ) >> 1 ); //PB6 = D7
     
     
     LCD_RS = 0x00;  //set PB7 register select pin to low
@@ -33,10 +33,10 @@ void LCD_CMD(unsigned long cmd) {
 
 //this function passes the data to the LCD
 void LCD_WRITE (char data) {
-    GPIO_PORTB_DATA_R |= (GPIO_PORTB_DATA_R & ~0x08) | ((data & 0x01 ) << 3 )  ; // pb3 == D0
-    GPIO_PORTC_DATA_R |= (GPIO_PORTC_DATA_R & ~0xF0) | ((data & 0x1E ) << 3 ); //pc 4,5,6,7 = D 1,2,3,4
-    GPIO_PORTD_DATA_R |= (GPIO_PORTD_DATA_R & ~0xC0) | ((data & 0x60 ) << 1 ); // PD 6,7 = D 5,6
-    GPIO_PORTF_DATA_R |= (GPIO_PORTF_DATA_R & ~0x10) | ((data & 0x80 ) >> 3 ); //PF4 = D7
+    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x08) | ((data & 0x01 ) << 3 ); //PB3 = D0
+    GPIO_PORTC_DATA_R = (GPIO_PORTC_DATA_R & ~0xF0) | ((data & 0x1E ) << 3 ); //PC4,5,6,7 = D1,2,3,4
+    GPIO_PORTD_DATA_R = (GPIO_PORTD_DATA_R & ~0xC0) | ((data & 0x60 ) << 1 ); //PD6,7 = D5,6
+    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x40) | ((data & 0x80 ) >> 1 ); //PB6 = D7
 
     LCD_RS = 0x80;  //set PB7 to high
     LCD_EN = 0x04;  //set the enable pin high
