3ed9458438507cd904119b0b62bdb0e395ca7a56
Hisham5005 <85290215+Hisham5005@users.noreply.github.com>
2021-06-06 01:56:16 +0200

    Add files via upload

--- LCD.c
+++ LCD.c
@@ -1,17 +1,17 @@
-
-#include "tm4c123gh6pm.h"
 #include "port_init.c"
 
-#define LCD_RS  (GPIO_PORTB_DATA_R )    //PB.7 for 
-#define LCD_EN  (GPIO_PORTB_DATA_R )   //PB.3 for enable 
+#define LCD_RS  (GPIO_PORTB_DATA_R) //PB7 - Register Select 
+#define LCD_EN  (GPIO_PORTB_DATA_R) //PB3 - Enable
+#define LCD_RW  (GPIO_PORTD_DATA_R) //PD0 - Read/Write
  
 
-/*function protoypes */
-void LCD_CMD(unsigned long cmd);
-void LCD_WRITE (char data);
-void Delay(void);
-void LCD_CURSOR (void);
+//function protoypes for initialization
 
+void LCD_CMD(char cmd);
+void LCD_DATA(char data);
+void LCD_NUM(int n);
+void LCD_String(char *str);
+void delay(unsigned long time);
 
 
 
@@ -19,41 +19,63 @@ void LCD_CURSOR (void);
 //this function passes the command to the LCD
 void LCD_CMD(unsigned long cmd) {
        
-    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x08) | ((cmd & 0x01 ) << 3 ); //PB3 = D0
-    GPIO_PORTC_DATA_R = (GPIO_PORTC_DATA_R & ~0xF0) | ((cmd & 0x1E ) << 3 ); //PC4,5,6,7 = D1,2,3,4
-    GPIO_PORTD_DATA_R = (GPIO_PORTD_DATA_R & ~0xC0) | ((cmd & 0x60 ) << 1 ); //PD6,7 = D5,6
-    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x40) | ((cmd & 0x80 ) >> 1 ); //PB6 = D7
+    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x08) | ((cmd & 0x01) << 3); //PB3 = D0
+    GPIO_PORTC_DATA_R = (GPIO_PORTC_DATA_R & ~0xF0) | ((cmd & 0x1E) << 3); //PC4,5,6,7 = D1,2,3,4
+    GPIO_PORTD_DATA_R = (GPIO_PORTD_DATA_R & ~0xC0) | ((cmd & 0x60) << 1); //PD6,7 = D5,6
+    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x40) | ((cmd & 0x80) >> 1); //PB6 = D7
     
     
     LCD_RS &= 0x7F;  //set PB7 register select pin to low
+    LCD_RW &= 0x00;  //clear PD0
     LCD_EN |= 0x04;  //set enable pin to high
-    Delay();        //short delay 
+    delay(30000);         //short delay 
     LCD_EN &= 0xFB;  //set enable pin to low 
 }
 
 //this function passes the data to the LCD
-void LCD_WRITE (char data) {
-    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x08) | ((data & 0x01 ) << 3 ); //PB3 = D0
-    GPIO_PORTC_DATA_R = (GPIO_PORTC_DATA_R & ~0xF0) | ((data & 0x1E ) << 3 ); //PC4,5,6,7 = D1,2,3,4
-    GPIO_PORTD_DATA_R = (GPIO_PORTD_DATA_R & ~0xC0) | ((data & 0x60 ) << 1 ); //PD6,7 = D5,6
-    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x40) | ((data & 0x80 ) >> 1 ); //PB6 = D7
+void LCD_DATA (char data) {
+    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x08) | ((data & 0x01) << 3); //PB3 = D0
+    GPIO_PORTC_DATA_R = (GPIO_PORTC_DATA_R & ~0xF0) | ((data & 0x1E) << 3); //PC4,5,6,7 = D1,2,3,4
+    GPIO_PORTD_DATA_R = (GPIO_PORTD_DATA_R & ~0xC0) | ((data & 0x60) << 1); //PD6,7 = D5,6
+    GPIO_PORTB_DATA_R = (GPIO_PORTB_DATA_R & ~0x40) | ((data & 0x80) >> 1); //PB6 = D7
 
     LCD_RS |= 0x80;  //set PB7 to high
+    LCD_RW &= 0x00;  //clear PD0
     LCD_EN |= 0x04;  //set the enable pin high
-    Delay();        //short delay
+    delay(30000);        //short delay
     LCD_EN &= 0xFB;  //set the enable pin to low
 }
 
-//short delay function
-void Delay(void) {
-    unsigned long  time;
-    time = 500; 
+// Controls LCD delay
+void delay(unsigned long  time) { //in microseconds
     while(time){
         time--;
     }
 }
 
-//LCD_cursor function to move the cursor to right
-void LCD_CURSOR (void){
-    LCD_CMD(0x06);
+
+//Allows writing strings on the LCD
+void LCD_String (char *phrase)
+{
+  int v;
+  for(v=0;phrase[v]!=0;v++)
+  {
+    LCD_DATA(phrase[v]);
+  }
 }
+
+
+//Allows writing numbers on the LCD
+void LCD_NUM(int x){
+    LCD_DATA((x/1000)+48); //Outputs thousands with ASCII offset
+    x %= 1000;
+
+    LCD_DATA((x/100)+48); //Outputs hundreds with ASCII offset
+    x %= 100;
+
+    LCD_DATA((x/10)+48); //Outputs tens with ASCII offset
+    x %= 10;
+
+    LCD_DATA(x+48); //Outputs ones with ASCII offset
+}
+
