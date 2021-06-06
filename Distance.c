827f5ea8ed32a6dc56e523a5f3829fb029b04f0c
yossef-Joe <85301193+yossef-Joe@users.noreply.github.com>
2021-06-06 02:13:01 +0200

    Add files via upload

--- Distance.c
+++ Distance.c
@@ -1,11 +1,11 @@
 #include "LCD.c"
-#define Green_LED GPIO_PORTF_DATA_R
+#define GreenLED GPIO_PORTF_DATA_R
 
 int main (void){
-  port_init();
-
   int Distance = 0;
   int Desired_Distance = 100;
+	
+  port_init();
 
 	LCD_CMD(0X38); //Setups 2 row mode
   LCD_CMD(0x06); //shifts display 1 digit to the left
@@ -15,12 +15,13 @@ int main (void){
   for(Distance=0; Distance<=9999; Distance++){ //main counter loop
   LCD_String ("Distance: ");
   LCD_NUM(Distance);
-  delay(1000000);
+  delay(5000000);
   LCD_CMD(0x01);
   
     if(Distance >= Desired_Distance){ //Turns green LED ON when the distance reaches the desired distance
-    Green_LED |= 0x08;
+    GreenLED |= 0x08;
     }
 
   }
 }
+
